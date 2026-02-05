55da8118b7ec134297e8e3d6482e29730669bc28
N3mphis <66238416+n3mphis@users.noreply.github.com>
2026-02-05 11:22:34 -0300

    Creado el archivo para crear las tablas de peliculas

---
+++ src/main/resources/db/migration/V1__create__table__movies.sql
@@ -0,0 +1,10 @@
+CREATE TABLE movies
+(
+    id              BIGSERIAL PRIMARY KEY,
+    imdb_id         VARCHAR(20) UNIQUE NOT NULL,
+    title           VARCHAR(255)       NOT NULL,
+    director        VARCHAR(255),
+    release_year    INT,
+    runtine_minutes INT,
+    plot            TEXT
+);
