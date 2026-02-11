f49ca58467c8496f008fb10326192905fd6f5fbe
N3mphis <66238416+n3mphis@users.noreply.github.com>
2026-02-11 10:37:02 -0300

    Creada la entidad Usuario

---
+++ src/main/java/com/cinema_paradiso/cinema/paradiso/model/Usuario.java
@@ -0,0 +1,33 @@
+package com.cinema_paradiso.cinema.paradiso.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El email no puede estar vacío")
+    @Email(message = "El formato del email no es válido")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+    private String password;
+
+    public Usuario(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
