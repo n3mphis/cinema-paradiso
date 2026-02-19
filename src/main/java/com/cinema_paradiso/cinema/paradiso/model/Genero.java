2fbafc10a9ec05fbb91cc022eba886bac08732f7
N3mphis <66238416+n3mphis@users.noreply.github.com>
2026-02-19 11:09:47 -0300

    Creado el Enum Genero para las peliculas

---
+++ src/main/java/com/cinema_paradiso/cinema/paradiso/model/Genero.java
@@ -0,0 +1,14 @@
+package com.cinema_paradiso.cinema.paradiso.model;
+
+public enum Genero {
+    ACCION,
+    DRAMA,
+    COMEDIA,
+    TERROR,
+    SUSPENSO,
+    MUSICAL,
+    ROMANCE,
+    CRIMEN,
+    WESTERN,
+    JURIDICO
+}
